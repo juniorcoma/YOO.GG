e6d0bd0651fe73d821106f1585aed1e43cc60d7f
parkhyeonwoo <pq120wal39@gmail.com>
2024-08-21 23:07:42 +0900

    response type 정의

--- src/constant/renderList.ts
+++ src/constant/renderList.ts
@@ -10,3 +10,27 @@ export const BOTTOM_NAV_RENDER_LIST = [
     path: '/champion-list',
   },
 ];
+
+export const FOOTER_SNS_RENDER_LIST = [
+  {
+    id: 1,
+    alt: 'instagram account',
+    imgPath: '/images/insta_logo.png',
+    link: 'https://www.instagram.com/pq120wal/',
+    size: 32,
+  },
+  {
+    id: 2,
+    alt: 'github account',
+    imgPath: '/images/github_logo.png',
+    link: 'https://github.com/juniorcoma',
+    size: 28,
+  },
+  {
+    id: 3,
+    alt: 'discord account',
+    imgPath: '/images/discord_logo.png',
+    link: '/',
+    size: 28,
+  },
+];
