e9ba3c38aff7780d06558cf6a558953cdd8aec43
parkhyeonwoo <pq120wal39@gmail.com>
2024-08-28 00:52:58 +0900

    소환사 리그 정보 타입 추가

--- src/types/response.ts
+++ src/types/response.ts
@@ -4,6 +4,21 @@ export interface AccountType {
   tagLine: string;
 }
 
+export interface LeagueDataType {
+  leagueId: string;
+  queueType: string;
+  tier: string;
+  rank: string;
+  summonerId: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  veteran: boolean;
+  inactive: boolean;
+  freshBlood: boolean;
+  hotStreak: boolean;
+}
+
 export interface SummonerDataType {
   accountId: string;
   profileIconId: number;
