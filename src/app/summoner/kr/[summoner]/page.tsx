5453b2dfcaa98bd9f289c3de6ea1ef8db446b0e6
parkhyeonwoo <pq120wal39@gmail.com>
2024-08-30 11:32:29 +0900

    page 컴포넌트 수정

--- src/app/summoner/kr/[summoner]/page.tsx
+++ src/app/summoner/kr/[summoner]/page.tsx
@@ -1,6 +1,5 @@
-import GameTypeControlBar from '@/components/GameTypeControlBar';
+import SummonerRecordListContainer from '@/components/summoner/SummonerRecordListContainer';
 import SummonerLeagueContainer from '@/components/SummonerLeagueContainer';
-import SummonerProfile from '@/components/SummonerProfile';
 import { SERVER_REQUEST_HOST } from '@/constant/API';
 import { backendRequest } from '@/service/axios';
 import { getSummonerInfoData } from '@/service/server.api';
@@ -13,6 +12,9 @@ export default async function SummonerPage({ params }: { params: { summoner: str
   return (
     <>
       <SummonerLeagueContainer summonerId={summonerData.id} />
+      <div className="flex-1">
+        <SummonerRecordListContainer puuid={summonerData.puuid} championData={championData} />
+      </div>
     </>
   );
 }
