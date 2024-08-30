105fcc8d656341a39e55fdc0a171fa9b75366ab1
parkhyeonwoo <pq120wal39@gmail.com>
2024-08-30 11:23:34 +0900

    전적 데이터 API 필터 기능 구현

--- src/app/api/v1/riot/lol/match/[puuid]/route.ts
+++ src/app/api/v1/riot/lol/match/[puuid]/route.ts
@@ -1,5 +1,8 @@
+import { GAME_TYPE_QUEUEID } from '@/constant';
 import { RIOT_REGIONAL_HOST, RIOT_REQUEST_ENDPOINT } from '@/constant/API';
+import { GAME_TYPE_RENDER_LIST } from '@/constant/renderList';
 import { riotRequest } from '@/service/axios';
+import { GameType } from '@/types';
 import { MatchDtoType } from '@/types/response';
 import { AxiosError } from 'axios';
 import { NextResponse } from 'next/server';
@@ -17,25 +20,50 @@ export async function GET(request: Request, context: { params: Params }) {
   const searchParams = new URL(request.url).searchParams;
   const { puuid } = context.params;
   const start = searchParams.get('start') || '0';
-  const type = searchParams.get('type') || 'normal';
+  const type = (searchParams.get('type') as GameType | undefined) || 'TOTAL';
   try {
-    const { data } = await riotRequest.get(
-      `${RIOT_REGIONAL_HOST.ASIA}${RIOT_REQUEST_ENDPOINT.MATCH_ID_LIST}${puuid}/ids?count=20&start=${start}&type=${type}`,
-    );
-    const matchData: MatchDtoType[] = [];
-
-    for (const matchId of data) {
+    if (type === 'TOTAL') {
+      return filterResponseData(puuid, start);
+    } else {
+      const queueId = GAME_TYPE_QUEUEID[type];
       const { data } = await riotRequest.get(
-        `${RIOT_REGIONAL_HOST.ASIA}${RIOT_REQUEST_ENDPOINT.MATCH_DETAIL_INFO}${matchId}`,
+        `${RIOT_REGIONAL_HOST.ASIA}${RIOT_REQUEST_ENDPOINT.MATCH_ID_LIST}${puuid}/ids?count=20&start=${start}&queue=${queueId}`,
       );
-      matchData.push(data);
-      await requestDelay(200);
-    }
+      const matchData: MatchDtoType[] = [];
 
-    return NextResponse.json(matchData, { status: 200 });
+      for (const matchId of data) {
+        const { data } = await riotRequest.get(
+          `${RIOT_REGIONAL_HOST.ASIA}${RIOT_REQUEST_ENDPOINT.MATCH_DETAIL_INFO}${matchId}`,
+        );
+        matchData.push(data);
+        await requestDelay(100);
+      }
+
+      return NextResponse.json(matchData, { status: 200 });
+    }
   } catch (error) {
     if (error instanceof AxiosError) {
       return NextResponse.json({ error: error.message, status: error.status }, { status: error.status });
     }
   }
+}
+
+async function filterResponseData(puuid: string, start: string) {
+  const matchData: MatchDtoType[] = [];
+  const queueId = [450, 490, 420, 440];
+  const { data } = await riotRequest.get(
+    `${RIOT_REGIONAL_HOST.ASIA}${RIOT_REQUEST_ENDPOINT.MATCH_ID_LIST}${puuid}/ids?count=20&start=${start}`,
+  );
+
+  for (const matchId of data) {
+    const { data } = await riotRequest.get(
+      `${RIOT_REGIONAL_HOST.ASIA}${RIOT_REQUEST_ENDPOINT.MATCH_DETAIL_INFO}${matchId}`,
+    );
+
+    if (queueId.includes(data.info.queueId)) {
+      matchData.push(data);
+    }
+    await requestDelay(100);
+  }
+  return NextResponse.json(matchData, { status: 200 });
 }
