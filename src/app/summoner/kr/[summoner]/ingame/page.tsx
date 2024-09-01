e02c8601bc7a27fde80c183cfec64a5620ec7a46
parkhyeonwoo <pq120wal39@gmail.com>
2024-09-01 19:43:11 +0900

    인게임 페이지 임시 구현

--- src/app/summoner/kr/[summoner]/ingame/page.tsx
+++ src/app/summoner/kr/[summoner]/ingame/page.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
 export default function SummonerIngamePage() {
-  return <div>인게임 페이지</div>;
+  return (
+    <div className="w-[108rem] m-auto bg-color-gray-00 rounded-[0.8rem] min-h-[35rem] flex justify-center items-center">
+      <div className="flex flex-col gap-[1.6rem] text-[2.4rem] items-center">
+        <div className="w-[12.8rem] aspect-square relative">
+          <Image src={'/images/em_bee_sad.png'} fill alt="벌꿀 이미지" />
+        </div>
+        <p>현재 인게임 기능을 사용할 수 없습니다.</p>
+      </div>
+    </div>
+  );
 }
