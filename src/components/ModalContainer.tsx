9526ce0623f46cfe3a6a3ec7f1a1c734459d8012
parkhyeonwoo <pq120wal39@gmail.com>
2024-09-02 19:33:29 +0900

    모달이 열렸을 때 페이지가 바뀌면 모달 닫히는 기능 구현

--- src/components/ModalContainer.tsx
+++ src/components/ModalContainer.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { ModalStateContext } from '@/hook/ModalProvider';
+import useModal from '@/hook/useModal';
+import { usePathname } from 'next/navigation';
 
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 export default function ModalContainer() {
   const { component: Component, props } = useContext(ModalStateContext);
+  const pathName = usePathname();
+  const { closeModal } = useModal();
+  const previousPathName = useRef(pathName);
   useEffect(() => {
     if (Component) {
       document.body.style.overflow = 'hidden';
@@ -16,6 +21,15 @@ export default function ModalContainer() {
       document.body.style.overflow = 'auto';
     };
   }, [Component]);
+
+  useEffect(() => {
+    if (Component && previousPathName.current !== pathName) {
+      closeModal();
+    }
+
+    previousPathName.current = pathName;
+  }, [pathName, Component, closeModal]);
+
   if (Component === null) return null;
 
   const portal = document.getElementById('portal') || document.createElement('div');
