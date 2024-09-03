8358b3eedb739b90b48a2f432286f3e298c93d3f
parkhyeonwoo <pq120wal39@gmail.com>
2024-09-03 11:09:25 +0900

    전적 데이터 query 수정 (데이터가 없을시 빈배열 반환)

--- src/hook/query/useGetSummonerRecordList.tsx
+++ src/hook/query/useGetSummonerRecordList.tsx
@@ -16,6 +16,9 @@ async function getSummonerRecordList({
   const { data } = await axios.get(
     `${SERVER_REQUEST_URL.SUMMONER_RECORD_DATA}${puuid}?start=${pageParam * 20}&type=${type}`,
   );
+  if (data.error) {
+    return [];
+  }
   return data;
 }
 
@@ -24,7 +27,12 @@ export default function useGetSummonerRecordList(puuid: string, type: GameType) 
     queryKey: ['summonerRecordInfo', puuid, type],
     queryFn: async ({ pageParam = 0 }) => await getSummonerRecordList({ pageParam, puuid, type }),
     initialPageParam: 0,
-    getNextPageParam: (lastPage: any, pages) => pages.length,
+    getNextPageParam: (lastPage: any, pages) => {
+      if (!lastPage || lastPage.length === 0) {
+        return undefined;
+      }
+      return pages.length;
+    },
     staleTime: 1000 * 60 * 10,
   });
 }
