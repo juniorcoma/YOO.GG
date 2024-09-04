bf630e0f101bb38888083a25f6d7e3bb758c753f
parkhyeonwoo <pq120wal39@gmail.com>
2024-09-04 22:29:06 +0900

    최적화 비활성화 옵션 취소

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true, // 이미지 최적화 비활성화
-  },
   reactStrictMode: true,
   webpack: config => {
     config.module.rules.push({
