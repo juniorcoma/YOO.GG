e2db0a4a1240277ba8f5bd673b93501672e0dc91
parkhyeonwoo <pq120wal39@gmail.com>
2024-09-09 01:51:31 +0900

    SummonerRecordServerContainer 롤백

--- src/components/SummonerRecordServerContainer.tsx
+++ src/components/SummonerRecordServerContainer.tsx
@@ -16,36 +16,18 @@ export default async function SummonerRecordServerContainer({
   puuid: string;
   gameType: GameType;
 }) {
-  try {
-    // 비동기 요청들을 병렬로 실행
-    const [championsData, versionsData, runesDataArr, initialRecordData, itemsData, summonerSpellsData] =
-      await Promise.all([
-        getChampionsData(),
-        getVersionsData(),
-        getRunesData(),
-        getInitialRecordData(puuid, gameType),
-        getItemsData(),
-        getSummonerSpellsData(),
-      ]);
-
-    const [latestVersion] = versionsData;
-
-    return (
-      <SummonerRecordClientContainer
-        data={{
-          champions: championsData,
-          runesArr: runesDataArr,
-          summonerSpells: summonerSpellsData,
-          items: itemsData,
-        }}
-        puuid={puuid}
-        initialRecordData={initialRecordData}
-        latestVersion={latestVersion}
-      />
-    );
-  } catch (error) {
-    console.error('Error fetching data:', error);
-
-    return <div>Error loading summoner data.</div>;
-  }
+  const championsData = await getChampionsData();
+  const [latestVersion] = await getVersionsData();
+  const runesDataArr = await getRunesData();
+  const initialRecordData = await getInitialRecordData(puuid, gameType);
+  const itemsData = await getItemsData();
+  const summonerSpellsData = await getSummonerSpellsData();
+  return (
+    <SummonerRecordClientContainer
+      data={{ champions: championsData, runesArr: runesDataArr, summonerSpells: summonerSpellsData, items: itemsData }}
+      puuid={puuid}
+      initialRecordData={initialRecordData}
+      latestVersion={latestVersion}
+    />
+  );
 }
