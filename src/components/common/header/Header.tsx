8e8e2aef2f6b83c50f451366cf01eec60f5f7787
parkhyeonwoo <pq120wal39@gmail.com>
2024-09-10 16:30:51 +0900

    상단 헤더 디자인 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import ContentSelectBox from './ContentSelecetBox';
 import Logo from '@/assets/icons/yoogg_logo.svg';
 import BottomHeader from './BottomHeader';
 import { getVersionsData } from '@/service/requestJsonData.api';
@@ -17,12 +16,11 @@ export default async function Header() {
 function TopHeader() {
   return (
     <div className="bg-[#1C1C1F] relative">
-      <div className="h-[4.8rem] flex items-center">
+      <div className="h-[4.8rem] flex items-center justify-center">
         <Link href="/" className="h-full px-[2.4rem] flex justify-center items-center">
           <h1 className="hidden-logo">YOO.GG</h1>
           <Logo />
         </Link>
-        <ContentSelectBox />
       </div>
     </div>
   );
