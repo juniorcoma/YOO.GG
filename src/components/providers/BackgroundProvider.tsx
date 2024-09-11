6c425187e8f3066b5f94117d0cb75bffde45dfa7
parkhyeonwoo <pq120wal39@gmail.com>
2024-09-11 14:56:57 +0900

    background providers 수정(다크 모드 전역 상태 추가)

--- src/components/providers/BackgroundProvider.tsx
+++ src/components/providers/BackgroundProvider.tsx
@@ -1,16 +1,41 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+import { createContext, useEffect, useState } from 'react';
+
+export const BackgroundColorState = createContext<{ state: boolean; setState: () => void } | null>(null);
 
 export default function BackgroundProvider({ children }: { children: React.ReactNode }) {
   const pathName = usePathname();
+  const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
-    const html = document.querySelector('html');
-    if (pathName !== '/' && html) {
-      html.style.backgroundColor = 'var(--color-gray-200)';
+    const isDark = localStorage.getItem('isDark') as ('true' | 'false') | undefined;
+    if (isDark) {
+      setIsDark(JSON.parse(isDark));
     }
-  }, [pathName]);
-  return <>{children}</>;
+  }, []);
+
+  useEffect(() => {
+    const html = document.querySelector('html') as HTMLHtmlElement;
+
+    if (html.classList.contains('dark')) {
+      html.style.backgroundColor = 'var(--color-gray-200)';
+    } else if (pathName !== '/') {
+      html.style.backgroundColor = 'var(--color-gray-200)';
+    } else if (pathName === '/') {
+      html.style.backgroundColor = 'var(--color-primary-500)';
+    }
+  }, [pathName, isDark]);
+
+  const setIsDarkmode = () => {
+    setIsDark(prev => !prev);
+    const isDarkState = JSON.stringify(!isDark);
+    localStorage.setItem('isDark', isDarkState);
+  };
+  return (
+    <BackgroundColorState.Provider value={{ state: isDark, setState: setIsDarkmode }}>
+      {children};
+    </BackgroundColorState.Provider>
+  );
 }
