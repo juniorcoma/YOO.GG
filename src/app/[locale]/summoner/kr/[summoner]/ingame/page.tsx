c81c2ced1ad02aa306147e70439a16bcdb8eaacf
parkhyeonwoo <pq120wal39@gmail.com>
2024-11-15 00:28:57 +0900

    Ingame 페이지 수정

--- src/app/[locale]/summoner/kr/[summoner]/ingame/page.tsx
+++ src/app/[locale]/summoner/kr/[summoner]/ingame/page.tsx
@@ -1,3 +1,6 @@
+import InGameContainer from '@/components/ingame/InGameContainer';
+import { getInGameData } from '@/service/requestJsonData.api';
+import { LanguageParamsType } from '@/types';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
@@ -8,16 +11,22 @@ export async function generateMetadata({ params }: { params: { summoner: string 
     title: `${summonerName} 인게임 정보`,
   };
 }
-export default function SummonerIngamePage() {
-  const t = useTranslations('ingamePage');
+export default async function SummonerIngamePage({
+  params,
+}: {
+  params: { summoner: string; locale: LanguageParamsType };
+}) {
+  const [name, tag] = params.summoner.split('-');
+
+  const inGameData = await getInGameData(name, tag);
+
   return (
-    <div className="w-[108rem] m-auto bg-color-gray-00 rounded-[0.8rem] min-h-[35rem] flex justify-center items-center">
-      <div className="flex flex-col gap-[1.6rem] text-[2.4rem] items-center">
-        <div className="w-[12.8rem] aspect-square relative">
-          <Image src={'/images/em_bee_sad.png'} fill alt="벌꿀 이미지" />
-        </div>
-        <p>{t('title')}</p>
-      </div>
+    <div
+      className={`w-[108rem] m-auto bg-color-gray-00 rounded-[0.8rem] min-h-[35rem] ${
+        !inGameData.ingame ? 'flex justify-center items-center' : ''
+      }`}
+    >
+      <InGameContainer inGameData={inGameData} locale={params.locale} />
     </div>
   );
 }
